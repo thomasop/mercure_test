aa8d0b0085291a4fdb738927e0328c67c16e7e4d
thomasop <tdss33@hotmail.com>
2022-05-13 10:30:06 +0200

    Add: Conversation Entity #8

---
+++ src/Entity/Conversation.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\UX\Turbo\Attribute\Broadcast;
+use App\Repository\ConversationRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+
+#[ORM\Entity(repositoryClass: ConversationRepository::class)]
+#[Broadcast]
+class Conversation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\OneToMany(targetEntity: Participant::class, mappedBy: 'conversation')]
+    private $participants;
+
+    
+    #[ORM\OneToOne(targetEntity: Message::class)]
+    #[ORM\JoinColumn(name: 'last_message_id', referencedColumnName: 'id')]
+    private $lastMessage;
+
+    #[ORM\OneToMany(targetEntity: Message::class, mappedBy: 'conversation')]
+    private $messages;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function __construct()
+    {
+        $this->participants = new ArrayCollection();
+        $this->messages = new ArrayCollection();
+    }
+
+    public function  __toString()
+    {
+        return $this->id;
+    }
+    /**
+     * @return Collection|Participant[]
+     */
+    public function getParticipants(): Collection
+    {
+        return $this->participants;
+    }
+
+    public function addParticipant(Participant $participant): self
+    {
+        if (!$this->participants->contains($participant)) {
+            $this->participants[] = $participant;
+            $participant->setConversation($this);
+        }
+
+        return $this;
+    }
+
+    public function removeParticipant(Participant $participant): self
+    {
+        if ($this->participants->contains($participant)) {
+            $this->participants->removeElement($participant);
+            // set the owning side to null (unless already changed)
+            if ($participant->getConversation() === $this) {
+                $participant->setConversation(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getLastMessage(): ?Message
+    {
+        return $this->lastMessage;
+    }
+
+    public function setLastMessage(?Message $lastMessage): self
+    {
+        $this->lastMessage = $lastMessage;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Message[]
+     */
+    public function getMessages(): Collection
+    {
+        return $this->messages;
+    }
+
+    public function addMessage(Message $message): self
+    {
+        if (!$this->messages->contains($message)) {
+            $this->messages[] = $message;
+            $message->setConversation($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMessage(Message $message): self
+    {
+        if ($this->messages->contains($message)) {
+            $this->messages->removeElement($message);
+            // set the owning side to null (unless already changed)
+            if ($message->getConversation() === $this) {
+                $message->setConversation(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Repository/ConversationRepository.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Conversation;
+use Doctrine\ORM\ORMException;
+use Doctrine\ORM\Query\Expr\Join;
+use Doctrine\ORM\Query\Parameter;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+
+/**
+ * @method Conversation|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Conversation|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Conversation[]    findAll()
+ * @method Conversation[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ConversationRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Conversation::class);
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function add(Conversation $entity, bool $flush = true): void
+    {
+        $this->_em->persist($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function remove(Conversation $entity, bool $flush = true): void
+    {
+        $this->_em->remove($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    public function findConversationByParticipants(int $otherUserId, $myId)
+    {
+        $qb = $this->createQueryBuilder('c');
+        $qb
+            ->select($qb->expr()->count('p.conversation'))
+            ->innerJoin('c.participants', 'p')
+            ->where(
+                $qb->expr()->orX(
+                    $qb->expr()->eq('p.user', ':me'),
+                    $qb->expr()->eq('p.user', ':otherUser')
+                )
+            )
+            ->groupBy('p.conversation')
+            ->having(
+                $qb->expr()->eq(
+                    $qb->expr()->count('p.conversation'),
+                    2
+                )
+            )
+            ->setParameters([
+                'me' => $myId,
+                'otherUser' => $otherUserId
+            ])
+        ;
+
+        return $qb->getQuery()->getResult();
+    }
+
+    public function findConversationsByUser($userId)
+    {
+        $qb = $this->createQueryBuilder('c');
+        $qb->
+            select('otherUser.username', 'c.id as conversationId', 'lm.content', 'lm.createdAt')
+            ->innerJoin('c.participants', 'p', Join::WITH, $qb->expr()->neq('p.user', ':user'))
+            ->innerJoin('c.participants', 'me', Join::WITH, $qb->expr()->eq('me.user', ':user'))
+            ->leftJoin('c.lastMessage', 'lm')
+            ->innerJoin('me.user', 'meUser')
+            ->innerJoin('p.user', 'otherUser')
+            ->where('meUser.id = :user')
+            ->setParameter('user', $userId)
+            ->orderBy('lm.createdAt', 'DESC')
+        ;
+        
+        return $qb->getQuery()->getResult();
+    }
+
+    public function searchParticipant($userId, $mots)
+    {
+        $qb = $this->createQueryBuilder('c');
+        $qb->
+            select('otherUser.username', 'c.id as conversationId', 'lm.content', 'lm.createdAt')
+            ->innerJoin('c.participants', 'p', Join::WITH, $qb->expr()->neq('p.user', ':user'))
+            ->innerJoin('c.participants', 'me', Join::WITH, $qb->expr()->eq('me.user', ':user'))
+            ->leftJoin('c.lastMessage', 'lm')
+            ->innerJoin('me.user', 'meUser')
+            ->innerJoin('p.user', 'otherUser')
+            ->where('MATCH_AGAINST(otherUser.username) AGAINST(:mots boolean)>0')
+            ->andWhere('meUser.id = :user')
+            ->setParameters(new ArrayCollection([
+                new Parameter('user', $userId),
+                new Parameter('mots','*' . $mots . '*'),
+            ]))
+            ->orderBy('lm.createdAt', 'DESC')
+        ;
+        
+        return $qb->getQuery()->getResult();
+    }
+
+    public function checkIfUserisParticipant(int $conversationId, $userId)
+    {
+        $qb = $this->createQueryBuilder('c');
+        $qb
+            ->innerJoin('c.participants', 'p')
+            ->where('c.id = :conversationId')
+            ->andWhere(
+                $qb->expr()->eq('p.user', ':userId')
+            )
+            ->setParameters([
+                'conversationId' => $conversationId,
+                'userId' => $userId
+            ])
+        ;
+
+        return $qb->getQuery()->getOneOrNullResult();
+    }
+
+    public function checkSearch($mots)
+    {
+        $qb = $this->createQueryBuilder('c');
+        $qb
+            ->Where(
+                $qb->expr()->eq('c.participants', ':current')
+            )
+            ->andWhere('MATCH_AGAINST(c.username) AGAINST(:mots boolean)>0')
+            ->setParameters([
+                'current' => $this->tokenStorageInterface->getToken()->getUser(),
+                'mots','*' . $mots . '*'
+            ])
+        ;
+
+        return $qb->getQuery()->getOneOrNullResult();
+    }
+    // /**
+    //  * @return Conversation[] Returns an array of Conversation objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('c.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Conversation
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
