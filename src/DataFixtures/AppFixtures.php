641385f8009fd43f186a275c0fc3943b4aaa32b0
thomasop <tdss33@hotmail.com>
2022-05-13 10:36:42 +0200

    Add: Fixture #9

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Post;
+use App\Entity\User;
+use App\Entity\Comment;
+use App\Entity\Message;
+use App\Entity\Conversation;
+use App\Entity\Participant;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    /** @var UserPasswordHasherInterface */
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $admin = new User();
+        $admin->setUsername('brumen');
+        $admin->setEmail('mail@mail.com');
+        $adminPassword = $this->passwordHasher->hashPassword(
+            $admin,
+            'Test1234?'
+        );
+        $admin->setPassword($adminPassword);
+        $admin->setRoles(["ROLE_ADMIN"]);
+        $manager->persist($admin);
+
+        $paul = new User();
+        $paul->setUsername('paul');
+        $paul->setEmail('paul@mail.com');
+        $paulPassword = $this->passwordHasher->hashPassword(
+            $paul,
+            'Test1234?'
+        );
+        $paul->setPassword($paulPassword);
+        $paul->setRoles(["ROLE_ADMIN"]);
+        $manager->persist($paul);
+
+
+        $manager->flush();
+    }
+}
